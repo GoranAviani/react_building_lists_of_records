3e94e555e13e0ab2dcce014153c1f80f23a8b70c
GoranAviani <goranaviani@gmail.com>
2022-04-04 10:51:30 +0200

    Building lists of records

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,35 @@
 import React from 'react';
 
+import unsplash from "../api/unsplash";
+import SearchBar from "./SearchBar";
 
-class App extends React.Component{
-
-    render(){
-
-        return(
-            <div>App</div>
-        )
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {images: []}
     }
 
+    onSearchSubmit = async (term) => {
+        const response = await unsplash.get('/search/photos',
+            {
+                params: {
+                    query: term
+                }
+            });
+        this.setState({images: response.data.results})
+    }
+
+
+    render() {
+        return (
+            <div>
+                <div className="ui container" style={{marginTop: '10px'}}>
+                    <SearchBar onSubmit={this.onSearchSubmit}/>
+                    Found: {this.state.images.length} images!
+                </div>
+            </div>
+        )
+    }
 }
 
 export default App;
